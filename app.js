5f62591b09ceedf5514b33b056da664214876c82
Seiya Sakaguchi <omprand.serbei@gmail.com>
2015-07-05 00:10:51 +0900

    Isolate routes.js from app.js.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
     connectHello,
     http     = require('http'),
     express  = require('express'),
+    routes = require('./routes'),
     app      = express(),
     server   = http.createServer(app);
 
@@ -27,40 +28,7 @@
     app.use(express.errorHandler());
   });
 
-  app.get('/', function(request, response) {
-    response.redirect('/index.html');
-  });
-
-  app.all('/:obj_type/*?', function(request, response, next) {
-    response.contentType('json');
-    next();
-  });
-
-  app.get('/:obj_type/list', function(request, response) {
-    response.send({title: request.params.obj_type + ' list'});
-  });
-
-  app.post('/:obj_type/create', function(request, response) {
-    response.send({title: request.params.obj_type + ' created'});
-  });
-
-  app.get('/:obj_type/read/:id([0-9]+)', function(request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-    });
-  });
-
-  app.post('/:obj_type/update:id([0-9]+)', function(request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
-    });
-  });
-
-  app.get('/:obj_type/delete:id([0-9]+)', function(request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-    });
-  });
+  routes.configRoutes(app, server);
 
   server.listen(3000);
   console.log(
