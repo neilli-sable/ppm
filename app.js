fac75b1c294e0f791f57a9ad972184a38d9c4e49
Seiya Sakaguchi <omprand.serbei@gmail.com>
2015-07-04 22:28:56 +0900

    Add common routes to app.js.

--- app.js
+++ app.js
@@ -31,6 +31,37 @@
     response.redirect('/index.html');
   });
 
+  app.all('/:obj_type/*?', function(request, response, next) {
+    response.contentType('json');
+    next();
+  });
+
+  app.get('/:obj_type/list', function(request, response) {
+    response.send({title: request.params.obj_type + ' list'});
+  });
+
+  app.post('/:obj_type/create', function(request, response) {
+    response.send({title: request.params.obj_type + ' created'});
+  });
+
+  app.get('/:obj_type/read/:id([0-9]+)', function(request, response) {
+    response.send({
+      title: request.params.obj_type + ' with id ' + request.params.id + ' found'
+    });
+  });
+
+  app.post('/:obj_type/update:id([0-9]+)', function(request, response) {
+    response.send({
+      title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
+    });
+  });
+
+  app.get('/:obj_type/delete:id([0-9]+)', function(request, response) {
+    response.send({
+      title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
+    });
+  });
+
   server.listen(3000);
   console.log(
     'Express serer listening on port %d in %s mode',
