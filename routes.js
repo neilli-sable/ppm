558f7e6391fb9fbf9be5afe4b55d77200741349d
Seiya Sakaguchi <omprand.serbei@gmail.com>
2015-07-05 20:21:39 +0900

    Make CRUD procedure.

--- routes.js
+++ routes.js
@@ -3,19 +3,24 @@
 */
 'use strict';
 var
+  makeMongoId,
   configRoutes,
-  mongoClient = require('mongodb').MongoClient,
+  mongodb     = require('mongodb'),
+  MongoClient = require('mongodb').MongoClient,
   assert      = require('assert'),
   url = "mongodb://localhost:27017/ppm",
-  users
+  database,
+  obj_type
   ;
 
-mongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log('** Connected to MongoDB **');
-
-  users = db.collection('users');
+  database = db;
+  //db.close(); // Is this need? Idon't understand.
 });
+
+makeMongoId = mongodb.ObjectID;
 
 configRoutes = function(app, server) {
   app.get('/', function(request, response) {
@@ -24,33 +29,73 @@ configRoutes = function(app, server) {
 
   app.all('/:obj_type/*?', function(request, response, next) {
     response.contentType('json');
+    obj_type = database.collection(request.params.obj_type);
     next();
   });
 
   app.get('/:obj_type/list', function(request, response) {
-    response.send({title: request.params.obj_type + ' list'});
+    obj_type.find().toArray(function(error, items) {
+      response.send(items);
+    });
   });
 
   app.post('/:obj_type/create', function(request, response) {
-    response.send({title: request.params.obj_type + ' created'});
+    var
+      options_map = {safe: true},
+      obj_map    = request.body;
+
+    obj_type.insert(
+      obj_map,
+      options_map,
+      function(error, result_map) {
+        response.send(result_map);
+      }
+    );
   });
 
-  app.get('/:obj_type/read/:id([0-9]+)', function(request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' found'
-    });
+  app.get('/:obj_type/read/:id', function(request, response) {
+    var find_map = {_id: makeMongoId(request.params.id)};
+
+    obj_type.findOne(
+      find_map,
+      function(error, result_map) {
+        response.send(result_map);
+      }
+    );
   });
 
-  app.post('/:obj_type/update:id([0-9]+)', function(request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' updated'
-    });
+  app.post('/:obj_type/update:id', function(request, response) {
+    var
+      find_map = {_id: makeMongoId(request.params.id)},
+      obj_map = request.body,
+      sort_order = [],
+      options_map = {
+        'new': true, upsert: false, safe: true
+      };
+
+    obj_type.findAndModify(
+      find_map,
+      sort_order,
+      obj_map,
+      options_map,
+      function(error, updated_map) {
+        response.send(updated_map);
+      }
+    );
   });
 
-  app.get('/:obj_type/delete:id([0-9]+)', function(request, response) {
-    response.send({
-      title: request.params.obj_type + ' with id ' + request.params.id + ' deleted'
-    });
+  app.get('/:obj_type/delete:id', function(request, response) {
+    var
+      find_map = {_id: makeMongoId(request.params.id)},
+      options_map = {safe: true, single: true};
+
+    obj_type.remove(
+      find_map,
+      options_map,
+      function(error, delete_count) {
+        response.send({delete_count: delete_count});
+      }
+    );
   });
 };
 
