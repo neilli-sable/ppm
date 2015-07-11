7e1f052efd9896e26878608fe5a7adfe5189bf5f
Seiya Sakaguchi <omprand.serbei@gmail.com>
2015-07-11 14:31:34 +0900

    Add error check when data creating or updating.

--- routes.js
+++ routes.js
@@ -1,17 +1,29 @@
 /*
  * routes.js
 */
+
+/*jslint         node    : true, continue : true,
+  devel  : true, indent  : 2,    maxerr   : 50,
+  newcap : true, nomen   : true, plusplus : true,
+  regexp : true, sloppy  : true, vars     : false,
+  white  : true
+*/
+/*global */
 'use strict';
 var
+  loadSchema, checkSchema,
+  fsHandle = require('fs'),
   makeMongoId,
   configRoutes,
   mongodb     = require('mongodb'),
   MongoClient = require('mongodb').MongoClient,
   assert      = require('assert'),
+  JSV         = require('JSV').JSV,
   url = "mongodb://localhost:27017/ppm",
   database,
-  obj_type
-  ;
+  obj_type,
+  objTypeMap = {'user': {}},
+  validator = JSV.createEnvironment();
 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
@@ -22,6 +34,22 @@ MongoClient.connect(url, function(err, db) {
 
 makeMongoId = mongodb.ObjectID;
 
+// ユーティリティメソッド開始
+loadSchema = function ( schema_name, schema_path ) {
+  fsHandle.readFile( schema_path, 'utf8', function ( err, data ) {
+    objTypeMap[ schema_name ] = JSON.parse( data );
+  });
+};
+
+checkSchema = function(obj_type, obj_map, callback) {
+  var
+    schema_map = objTypeMap[obj_type],
+    report_map = validator.validate(obj_map, schema_map);
+
+  callback(report_map.errors);
+};
+// ユーティリティメソッド終了
+
 configRoutes = function(app, server) {
   app.get('/', function(request, response) {
     response.redirect('/index.html');
@@ -29,8 +57,14 @@ configRoutes = function(app, server) {
 
   app.all('/:obj_type/*?', function(request, response, next) {
     response.contentType('json');
-    obj_type = database.collection(request.params.obj_type);
-    next();
+    if (objTypeMap[request.params.obj_type]) {
+      obj_type = database.collection(request.params.obj_type);
+      next();
+    }
+    else {
+      response.send({error_msg: request.params.obj_type +
+      'is not a valid object type'});
+    }
   });
 
   app.get('/:obj_type/list', function(request, response) {
@@ -40,15 +74,27 @@ configRoutes = function(app, server) {
   });
 
   app.post('/:obj_type/create', function(request, response) {
-    var
-      options_map = {safe: true},
-      obj_map    = request.body;
+    var obj_map    = request.body;
 
-    obj_type.insert(
-      obj_map,
-      options_map,
-      function(error, result_map) {
-        response.send(result_map);
+    checkSchema(
+      request.params.obj_type, obj_map,
+      function(error_list) {
+        if (error_list.length === 0) {
+          var options_map = {safe: true};
+          obj_type.insert(
+            obj_map,
+            options_map,
+            function(error, result_map) {
+              response.send(result_map);
+            }
+          );
+        }
+        else {
+          response.send({
+            error_msg: 'Input document not valid',
+            error_list: error_list
+          });
+        }
       }
     );
   });
@@ -64,22 +110,36 @@ configRoutes = function(app, server) {
     );
   });
 
-  app.post('/:obj_type/update:id', function(request, response) {
+  app.post('/:obj_type/update/:id', function(request, response) {
     var
       find_map = {_id: makeMongoId(request.params.id)},
-      obj_map = request.body,
-      sort_order = [],
-      options_map = {
-        'new': true, upsert: false, safe: true
-      };
+      obj_map = request.body;
 
-    obj_type.findAndModify(
-      find_map,
-      sort_order,
-      obj_map,
-      options_map,
-      function(error, updated_map) {
-        response.send(updated_map);
+    checkSchema(
+      request.params.obj_type, obj_map,
+      function(error_list) {
+        if (error_list.length === 0) {
+          var
+            sort_order = [],
+            options_map = {
+              'new': true, upsert: false, safe: true
+            };
+          obj_type.findAndModify(
+            find_map,
+            sort_order,
+            obj_map,
+            options_map,
+            function(error, updated_map) {
+              response.send(updated_map);
+            }
+          );
+        }
+        else {
+          response.send({
+            error_msg: 'Input document not valid',
+            error_list: error_list
+          });
+        }
       }
     );
   });
@@ -102,3 +162,13 @@ configRoutes = function(app, server) {
 module.exports = {
   configRoutes: configRoutes
 };
+
+(function () {
+  var schema_name, schema_path;
+  for ( schema_name in objTypeMap ) {
+    if ( objTypeMap.hasOwnProperty( schema_name ) ) {
+      schema_path = __dirname + '/' + schema_name + '.json';
+      loadSchema( schema_name, schema_path );
+    }
+  }
+}());
